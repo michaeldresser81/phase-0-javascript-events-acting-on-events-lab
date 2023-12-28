f6f11bd3462e5ad69525864e7069d01c775adfdc
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2023-12-28 18:26:45 -0500

    complted assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.querySelector("#dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+document.addEventListener("keydown", (event) => {
+    switch(true) {
+        case event.key === "ArrowLeft":
+            moveDodgerLeft();
+            break;
+        case event.key === "ArrowRight": 
+            moveDodgerRight();
+            break;
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left >= 5) {
+    dodger.style.left = `${left - 5}px`;
+    }
+}
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left <= 355) {
+    dodger.style.left = `${left + 5}px`;
+    }
+}
